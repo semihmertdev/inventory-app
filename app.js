274ae1ea0efd4e4603512ea59234f595fa99c81d
semihmertdev <semihmert.dev@gmail.com>
2024-08-18 01:13:48 +0300

    chore: Update app.js and home.ejs for serving static files and update path to styles.css

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const db = require('./db');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Add this line to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const categoriesRouter = require('./routes/categories');
