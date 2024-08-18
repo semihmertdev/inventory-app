fd77f89833322e420d7e28b1a662f83966562274
semihmertdev <semihmert.dev@gmail.com>
2024-08-18 15:09:04 +0300

    chore: Update navigation section in category and item views

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
-// script.js
-
 function toggleForm(formId) {
-    const form = document.getElementById(formId);
-    form.classList.toggle('show');
+  const form = document.getElementById(formId);
+  if (form.classList.contains('active')) {
+    form.classList.remove('active');
+  } else {
+    // Close any open forms
+    document.querySelectorAll('.collapsible-form.active').forEach(activeForm => {
+      activeForm.classList.remove('active');
+    });
+    // Open the selected form
+    form.classList.add('active');
   }
-  
+}
